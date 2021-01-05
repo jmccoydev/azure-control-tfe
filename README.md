7f5d020ef6b640dcc7e08b764285a15f8a433670
Adam Kentosh <akentosh@gmail.com>
2021-01-05 10:09:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Create an Azure Workspace in TFE/TFC
---
+++ variable.tf
@@ -0,0 +1,27 @@
+# Terraform Enterprise Variables
+variable "tfe_api_key" {
+  description = "Terraform Enterprise/Cloud API used to connect and configure workspaces"
+}
+variable "org_name" {
+  description = "Terraform Enterprise/Cloud Organization name to create the workspaces in"
+}
+variable "oauth_token_id" {
+  description = "Terraform Enterprise/Cloud VCS oauth token ID (found in settings/VCS)"
+}
+
+# Azure Variables
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+}
+
+variable "azure_client_id" {
+  description = "Azure Client ID"
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+}
+
+variable "azure_client_secret" {
+  description = "Azure Clinet Secret"
+}
